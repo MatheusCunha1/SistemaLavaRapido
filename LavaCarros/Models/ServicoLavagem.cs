de76eb63b0e76aeaea801b5789dc41c1456853c5
Matheus Cunha <104787089+MatheusCunha1@users.noreply.github.com>
2023-12-13 20:04:57 -0300

    Delete LavaCarros directory

--- LavaCarros/Models/ServicoLavagem.cs
+++
@@ -1,9 +0,0 @@
-public class ServicoLavagem
-{
-    public int Id { get; set; }
-    public string? Tipo { get; set; }
-    public string? Detalhes { get; set; }
-    public decimal Valor { get; set; }
-
-    public string? TipoLavagem { get; set; }
-}
